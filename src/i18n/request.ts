79cabf15f9b9e6824a63a1b22936cb0acc4197f5
undie7 <henryjunderwood@gmail.com>
2026-08-05 15:25:51 +0700

    fix: add i18n routing config for next-intl

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,7 +1,8 @@
 import {getRequestConfig} from 'next-intl/server';
+import {routing} from './routing';
 
 export default getRequestConfig(async ({requestLocale}) => {
-  const locale = (await requestLocale) ?? 'en';
+  const locale = (await requestLocale) ?? routing.defaultLocale;
   return {
     locale,
     messages: (await import(`../messages/${locale}.json`)).default
