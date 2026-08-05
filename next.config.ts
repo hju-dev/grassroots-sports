b62065d26aaf039153b3837efd97330bdbaa3b5f
Henry Underwood <142295519+hju-dev@users.noreply.github.com>
2026-08-05 15:17:36 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
-import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default withNextIntl(nextConfig);
